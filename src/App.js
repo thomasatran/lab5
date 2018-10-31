4bca48e6d2bfafed8fdbb44f83cbd0fb7a39ca42
Thomas Tran <tat022@ucsd.edu>
2018-10-31 10:24:37 -0700

    mes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TeamMembers from "./components/TeamMembers";
+import DisplayData from './components/DisplayData.js';
+import firebase from 'firebase';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        var config = {
+            apiKey: "AIzaSyCirM6SXBjFUM5a2liPibL86p4abK8x-2U",
+            authDomain: "lab4-d9c2b.firebaseapp.com",
+            databaseURL: "https://lab4-d9c2b.firebaseio.com",
+            projectId: "lab4-d9c2b",
+            storageBucket: "lab4-d9c2b.appspot.com",
+            messagingSenderId: "421384606415"
+        };
+        firebase.initializeApp(config);
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <TeamMembers db={firebase}/>
+          <DisplayData db={firebase}/>
       </div>
     );
   }
